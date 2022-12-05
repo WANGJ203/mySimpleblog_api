eee062cb1e27fcf774fd1848a84e733d1cb9efe5
Jiqiang WANG_Robert <110020364+WANGJ203@users.noreply.github.com>
2022-12-06 00:18:42 +1300

    set cors

--- simpleblog_api/settings.py
+++ simpleblog_api/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'rest_framework',
     'blog',
     'rest_framework.authtoken',
+    'corsheaders',
 ]
 
 # AllowAny权限类将允许不受限制的访问，而不管该请求是否已通过身份验证或未经身份验证。
@@ -51,6 +52,7 @@ REST_FRAMEWORK = {
 # }
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -129,3 +131,6 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CORS_ALLOW_ALL_ORIGINS = True  # If this is used then `CORS_ALLOWED_ORIGINS` will not have any effect
+CORS_ALLOW_CREDENTIALS = True
